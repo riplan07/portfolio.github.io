437891e9c97d1033e3a51bd7ac28d892b632cc20
Riplan Ahi <ahi@ozzi.tech>
2021-03-01 19:11:18 +0530

    adding nav and dropdown

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function myFunction() {
   var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  if (x.className === "top-nav") {
     x.className += " responsive";
   } else {
-    x.className = "topnav";
+    x.className = "top-nav";
   }
 }
